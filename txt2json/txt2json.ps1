42bfabed7c09e047321f7c2b6e910daac779f9e7
Carlos Mena <50986837+carlosm00@users.noreply.github.com>
2022-08-08 23:29:48 +0200

    Created

---
+++ txt2json/txt2json.ps1
@@ -0,0 +1,8 @@
+<#
+   .Description
+   Project: TXT 2 JSON
+   Author/Autor: Carlos Mena (https://github.com/carlosm00)
+   Definition: PowerShell Script for creating a TXT file to JSON format (https://github.com/carlosm00/simple-toolkit).
+
+#>
+
