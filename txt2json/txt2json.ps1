38133be29f774695b4228b1406c4b01e3c32b8d6
Carlos Mena <50986837+carlosm00@users.noreply.github.com>
2024-01-27 01:44:46 +0100

    added modularity

--- txt2json/txt2json.ps1
+++ txt2json/txt2json.ps1
@@ -5,4 +5,26 @@
    Definition: PowerShell Script for creating a TXT file to JSON format (https://github.com/carlosm00/simple-toolkit).
 
 #>
+function txt2json {
+   param (
+        [string]$inputFile,
+        [string]$outputFile
+    )
 
+   try {
+      $content = Get-Content -Path $inputFile -Raw
+
+      $psObject = ConvertFrom-Json $content
+      $jsonOut = $psObject | ConvertTo-Json
+
+      $jsonOut | Set-Content -Path $outputFile
+
+   }
+   catch {
+      $message = $_.Exception.message
+      $error_type = $_.Exception.GetType()
+      Write-Output "$error_type : $message"
+   }
+}
+
+txt2json -inputFile "$args[0]" -outputFile "$args[1]"
